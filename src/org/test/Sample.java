3529f04ee8af45d1c50b736ad4b64e51149d31b8
manickavin <manickavin07@gmail.com>
2022-02-23 12:28:08 +0530

    A branch is updated

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,15 +1,19 @@
 package org.test;
 
 public class Sample {
-	
-	
+
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("Java");
-		
+
 		System.out.println("Python");
-		
+
+		System.out.println("123456");
+
+		int a = 10;
+
+		System.out.println(a);
+
 	}
 
 }
